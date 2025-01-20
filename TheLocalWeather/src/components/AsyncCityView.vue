0abff09e24f87e3625dd7bc0a58fb11476798550
Sam Wei <sam90315@gmail.com>
2025-01-20 23:55:31 +0800

    API現在沒法使用QQ

--- TheLocalWeather/src/components/AsyncCityView.vue
+++ TheLocalWeather/src/components/AsyncCityView.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="flex flex-col flex-1 item-center">
-    <div
-      v-if="route.query.preview"
-      class="text-white p-4 bg-weather-secondary w-full text-center"
-    >
+    <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
       <p>
         You are currently previewing this city, click the "+" icon to start
         tracking this city.
@@ -34,11 +31,8 @@
         <p class="capitalize">
           {{ weatherData.current.weather[0].description }}
         </p>
-        <img
-          class="w-[150px] h-auto"
-          :src="`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
-          alt=""
-        />
+        <img class="w-[150px] h-auto"
+          :src="`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`" alt="" />
       </div>
     </div>
     <hr class="border-white border-opacity-10 border w-full" />
@@ -47,11 +41,7 @@
       <div class="mx-8 text-white">
         <h2 class="mb-7">Hourly Weather</h2>
         <div class="flex gap-10 overflow-x-scroll">
-          <div
-            v-for="hourData in weatherData.hourly"
-            :key="hourData.dt"
-            class="flex flex-col gap-4 items-center"
-          >
+          <div v-for="hourData in weatherData.hourly" :key="hourData.dt" class="flex flex-col gap-4 items-center">
             <p class="whitespace-nowrap text-md">
               {{
                 new Date(hourData.currentTime).toLocaleTimeString("en-us", {
@@ -59,11 +49,8 @@
                 })
               }}
             </p>
-            <img
-              class="w-auto h-[50px] object-cover"
-              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
-              alt=""
-            />
+            <img class="w-auto h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`" alt="" />
             <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
           </div>
         </div>
@@ -76,11 +63,7 @@
       <div class="mx-8 text-white">
         <h2 class="mb-4">7 Day Forecast</h2>
 
-        <div
-          v-for="day in weatherData.daily"
-          :key="day.dt"
-          class="flex items-center"
-        >
+        <div v-for="day in weatherData.daily" :key="day.dt" class="flex items-center">
           <p class="flex-1">
             {{
               new Date(day.dt * 1000).toLocaleDateString("en-us", {
@@ -88,11 +71,8 @@
               })
             }}
           </p>
-          <img
-            class="w-[50px] h-[50px] object-cover"
-            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
-            alt=""
-          />
+          <img class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt="" />
           <div class="flex gap-2 flex-1 justify-end">
             <p>
               {{ Math.round(day.temp.min) }}&deg; ~{{
@@ -103,10 +83,8 @@
         </div>
       </div>
     </div>
-    <div
-      class="flex items-center justify-center pb-12 gap-2 text-white cursor-pointer duration-150 hover:text-red-500"
-      @click="removeCity"
-    >
+    <div class="flex items-center justify-center pb-12 gap-2 text-white cursor-pointer duration-150 hover:text-red-500"
+      @click="removeCity">
       <i class="fa-solid fa-trash"></i>
       <p>RemoveCity</p>
     </div>
@@ -119,12 +97,12 @@ import { useRoute, useRouter } from "vue-router";
 const apiKey = import.meta.env.VITE_APP_WEATHER_KEY;
 const route = useRoute();
 const getWeatherData = async () => {
+  //這API現在只有3.0 3.0要錢 所以沒法
   try {
     const weatherData = await axios.get(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=${apiKey}
 &units=metric`
     );
-
     // cal current date & time
     const localOffset = new Date().getTimezoneOffset() * 60000;
     const utc = weatherData.data.current.dt * 1000 + localOffset;
